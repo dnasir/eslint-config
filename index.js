dc74c5c7fa07e2dcc1e9f877b597d0f22e54fe2e
Dzulqarnain Nasir <dzul@geta.no>
2019-11-06 16:36:44 +0200

    Updated index.js

--- index.js
+++ index.js
@@ -1,63 +1,56 @@
 module.exports = {
-  "parserOptions": {
-    "ecmaVersion": 2018,
-    "sourceType": "module",
-    "ecmaFeatures": {
-      "jsx": true
-    }
-  },
-  "env": {
-    "browser": true,
-    "es6": true,
-    "jest": true
-  },
-  "plugins": ["react", "prettier", "@typescript-eslint", "react-hooks"],
-  "extends": [
-    "eslint:recommended",
-    "plugin:import/recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-    "prettier/react",
-    "prettier/@typescript-eslint"
-  ],
-  "settings": {
-    "react": {
-      "version": "detect"
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module'
     },
-    "import/resolver": {
-      "node": {
-        "extensions": [
-          ".js",
-          ".jsx",
-          ".ts",
-          ".tsx"
-        ]
-      }
-    }
-  },
-  "rules": {
-    "prettier/prettier": [
-      0,
-      {
-        "singleQuote": true,
-        "trailingComma": "es5",
-        "printWidth": 100,
-        "tabWidth": 4,
-        "bracketSpacing": true
-      }
+    env: {
+        browser: true,
+        es6: true,
+        jest: true
+    },
+    plugins: ['@typescript-eslint', 'prettier'],
+    extends: [
+        'eslint:recommended',
+        'plugin:import/recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+        'prettier/@typescript-eslint'
     ],
-    "@typescript-eslint/interface-name-prefix": [2, {
-      "prefixWithI": "always",
-      "allowUnderscorePrefix": false
-    }],
-    "import/no-unresolved": 0,
-    "@typescript-eslint/explicit-function-return-type": 0,
-    "no-extra-boolean-cast": 0,
-    "import/no-named-as-default": 0,
-    "@typescript-eslint/no-inferrable-types": 0,
-    "import/no-duplicates": 0,
-    "import/named": 0
-  }
+    settings: {
+        'import/resolver': {
+            node: {
+                extensions: ['.js', '.ts']
+            }
+        }
+    },
+    rules: {
+        'prettier/prettier': [
+            2,
+            {
+                singleQuote: true,
+                trailingComma: 'none',
+                printWidth: 100,
+                tabWidth: 4,
+                bracketSpacing: true,
+                semi: true,
+                htmlWhitespaceSensitivity: 'strict'
+            }
+        ],
+        '@typescript-eslint/interface-name-prefix': [
+            2,
+            {
+                prefixWithI: 'always',
+                allowUnderscorePrefix: false
+            }
+        ],
+        '@typescript-eslint/explicit-function-return-type': 0,
+        '@typescript-eslint/no-inferrable-types': 0,
+        'import/no-unresolved': 0,
+        'no-extra-boolean-cast': 0,
+        'import/no-named-as-default': 0,
+        'import/no-duplicates': 0,
+        'import/named': 0
+    }
 };
